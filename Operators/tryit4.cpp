8467bfd570121329a04d2bd5d9cdab50cf621452
suicidesilence73 <160760183+suicidesilence73@users.noreply.github.com>
2024-02-25 11:18:05 -0500

    Add files via upload

---
+++ Operators/tryit4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	
+int x = 5763;
+int y = 9823;
+int z = 5677;
+cout << x/y/z ;
+	return 0;
+}
