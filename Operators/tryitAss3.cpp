b7ced33711a409f18d7c1b3ed068797c07b2ec21
suicidesilence73 <160760183+suicidesilence73@users.noreply.github.com>
2024-02-25 12:42:49 -0500

    Add files via upload

---
+++ Operators/tryitAss3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+
+	int x = 567;
+	x/= 90;
+	cout << x;
+	return 0;
+}
