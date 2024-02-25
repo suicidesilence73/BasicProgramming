b82b13dedc6be258fbb98723b5db91001e45485c
suicidesilence73 <160760183+suicidesilence73@users.noreply.github.com>
2024-02-25 14:42:19 -0500

    Add files via upload

---
+++ DataTypes/Datatype1.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+   int myNum = 300 +90;
+   double myDoubleNum = 5.96;
+   char myLetter = 'S'; 
+   bool myBoolean = true;
+   float myFloatNum = 5.99;
+   string myString = "WORD";
+   
+   
+  cout << "int: " << myNum << "\n";
+  cout << "float: " << myFloatNum << "\n";
+  cout << "double: " << myDoubleNum << "\n";
+  cout << "char: " << myLetter << "\n";
+  cout << "bool: " << myBoolean << "\n";
+  cout << "string: " << myString << "\n";
+  return 0;
+}
