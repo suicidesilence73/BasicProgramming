66ab7cc36628538c268c6c9843bf93633d966fc5
suicidesilence73 <160760183+suicidesilence73@users.noreply.github.com>
2024-02-25 12:45:13 -0500

    Add files via upload

---
+++ Operators/tryitAss4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+
+	int x =6789;
+	x=x% 456;
+	cout << x;
+	return 0;
+}
