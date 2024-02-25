7043756a1cc23a936dec5041b73c5a3f537aedbf
suicidesilence73 <160760183+suicidesilence73@users.noreply.github.com>
2024-02-25 15:29:05 -0500

    Add files via upload

---
+++ DataTypes/Characters2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main () {
+  char  P = 'P', A = 'A',T = 'T', O = 'O' ;
+  
+  cout << P;
+  cout << A;
+  cout << T;
+  cout << O;
+  
+  return 0;
+}
