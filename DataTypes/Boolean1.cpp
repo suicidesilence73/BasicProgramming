89d5420bf8bd132495041398260da175cb6fcded
suicidesilence73 <160760183+suicidesilence73@users.noreply.github.com>
2024-02-25 15:12:32 -0500

    Add files via upload

---
+++ DataTypes/Boolean1.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+   bool isUWUtasty = false;
+   bool isOWOtic  = false;
+   cout <<  isUWUtasty << "\n";
+   cout << isOWOtic ;
+   return 0;
+}
