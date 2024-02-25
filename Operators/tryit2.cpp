fa65628fe9c4ade0a08b71d6fc33f275cc365370
suicidesilence73 <160760183+suicidesilence73@users.noreply.github.com>
2024-02-25 10:56:31 -0500

    Add files via upload

---
+++ Operators/tryit2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+
+using namespace std;
+
+  int x = 9000;
+  int y = 5678;
+  cout << x - y;
+  return 0;
+}
+
+
