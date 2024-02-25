bca3406b49c005151ec66f0b0cf5bc7565efce66
suicidesilence73 <160760183+suicidesilence73@users.noreply.github.com>
2024-02-25 14:10:35 -0500

    Add files via upload

---
+++ Operators/tryitLog3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	int x = 78;
+	int y = 57;
+    cout << !(x > 76 && x < 67);
+	return 0;
+}
