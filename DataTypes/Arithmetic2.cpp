826d37ce612bc6151e87d465e2e20e5a430febc0
suicidesilence73 <160760183+suicidesilence73@users.noreply.github.com>
2024-02-25 10:34:45 -0500

    Add files via upload

---
+++ DataTypes/Arithmetic2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std; 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int sum1= 1000 + 25670;
+	int sum2= sum1 + 566;
+	int sum3= sum2 + sum2;
+	cout << sum1 << "\n";
+  cout << sum2 << "\n";
+  cout << sum3;
+	
+	
+	
+	
+	
+	
+	return 0;
+}
