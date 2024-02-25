9b9dd34430a2a2bcf8b6dba7950fa59cb2594e47
suicidesilence73 <160760183+suicidesilence73@users.noreply.github.com>
2024-02-25 13:33:41 -0500

    Add files via upload

---
+++ Operators/tryitCom2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 3;
+  int z = 3;
+  cout << (x!= y ==z); // returns 0 (false) because 5 is not equal to 3
+  return 0;
+}
+
