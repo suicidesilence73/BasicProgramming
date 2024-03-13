1532c506bf3f48b037e0dec40521e7d704b90f3f
suicidesilence73 <160760183+suicidesilence73@users.noreply.github.com>
2024-03-12 23:37:14 -0500

    Add files via upload

---
+++ loop/loop1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+
+    for (int i=0; i<9; i++){
+  cout << i << endl;
+}
+
+    return 0;
+//es como decir que no se haga mas repeticion si no se quedaria en bucle, y ademas se sale del codigo
+}
